94c6897ac4cc332dd802b00be5521d9e1239e446
YOUR NAME <YOUR@EMAIL.com>
2020-02-13 21:00:27 -0500

    property catalogue 02/13/2020

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,12 +1,45 @@
 class PropertiesController < ApplicationController
     before_action :authenticate_user!
     def index
-        @properties = Properties.all
+        @properties = current_user.properties
     end
 
     def show
+       @property = Property.find(params[:id])
+    end
+
+    def new
+        @property = Property.new 
+    end
+
+    def create
+        @property = current_user.properties.new(property_params)
+        
+        if @property.save
+            redirect_to @property, notice: 'Property was successfully saved'
+        else
+            render :new
+        end
+    end
+
+    def destroy
+        @property = Property.find(params[:id])
+        @property.destroy
+        redirect_to properties_url, notice: 'Property was successfully destroyed'
+    end
+
+    def edit
         @property = Property.find(params[:id])
     end
 
-    def new 
+    def update
+        property = Property.find(params["id"])
+        property.update(property_params)
+        redirect_to property    
+    end
+
+    private
+    def property_params
+            params.require(:property).permit(:name, :bedrooms, :bathrooms, :parking_spaces, :information, :user_id, :image)
+    end
 end
