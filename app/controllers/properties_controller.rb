9b198ace73a6d55d6b4ef35bb1e894e62eaef400
YOUR NAME <YOUR@EMAIL.com>
2020-02-17 19:04:13 -0500

    fixed authenticate user issue

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,5 @@
 class PropertiesController < ApplicationController
-    before_action :authenticate_user!
+    before_action :authenticate_user!, except: [:index]
     def index
         @properties = Property.all
     end
